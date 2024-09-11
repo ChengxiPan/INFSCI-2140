e2688b60de358f0613b55d6cfeaf1c52f4d728e3
chris <sweetchris1196@gmail.com>
2024-09-11 12:13:36 -0400

    update Path

--- Assignment1/Classes/Path.py
+++ Assignment1/Classes/Path.py
@@ -1,15 +1,15 @@
 # you should specify and put all files in the corresponding directory
 # address of target corpus docset.trectext.
-DataTextDir = "data//output//docset.trectext"
+DataTextDir = "data//input//docset.trectext"
 
 # address of target corpus docset.trecweb.
-DataWebDir = "data//output//docset.trecweb"
+DataWebDir = "data//input//docset.trecweb"
 
 # address of stopword list.
 StopwordDir="data//input//stopwords.txt"
 
 # uncompleted address of preprocessed corpus.
-ResultHM1="data//result."
+ResultHM1="data//output//result."
 
 # address of generated Text index file.
 IndexTextDir="data//indextext//"
