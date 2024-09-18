88ac36a680fb5d344a18acfbcf5b2e2ad2d0ad5e
chris <sweetchris1196@gmail.com>
2024-09-18 16:44:43 -0400

    Assignment2 init

---
+++ Assignment2/Indexing/PreProcessedCorpusReader.py
@@ -0,0 +1,11 @@
+import Classes.Path as Path
+
+# Please explain the code with necessary comments.
+class PreprocessedCorpusReader:
+
+    def __init__(self, type):
+        return
+
+    # Read a line for docNo from the corpus, read another line for the content, and return them in [docNo, content].
+    def nextDocument(self):
+        return
