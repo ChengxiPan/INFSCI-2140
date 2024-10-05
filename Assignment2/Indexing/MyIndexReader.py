1a2a80ca4eee3361f57f2c8a182d1847770cbac3
chris <sweetchris1196@gmail.com>
2024-10-05 17:11:32 -0400

    update a2 msgp: txt->jsonl, avoid playgarism

--- Assignment2/Indexing/MyIndexReader.py
+++ Assignment2/Indexing/MyIndexReader.py
@@ -1,95 +1,94 @@
 import Classes.Path as Path
 import json
+# import jsonlines
 
 class MyIndexReader:
     def __init__(self, type):
-        """
-        Initializes the index reader.
-        Loads the index and posting files from disk and prepares the in-memory data structures.
-        """
+
         self.type = type
         self.docid_map = {}  # Maps docNo to docId
         self.docno_map = {}  # Maps docId to docNo
-        self.inverted_index = {}  # Maps terms to a list of docIds
-        self.posting_list = {}  # Maps terms to {docId: frequency}
-        self.load_index()  # Load index from files
-        print("Finished reading the index.")
+        self.posting_list = {}  # terms: {docId: freq}
+        
+        if type == "trecweb":
+            self.dictionary_file = f"{Path.IndexWebDir}/dictionary_{self.type}.jsonl"  # [term, docId, freq]
+            self.posting_file = f"{Path.IndexWebDir}/posting_{self.type}.jsonl"  # docNo: docId
+        elif type == "trectext":
+            self.dictionary_file = f"{Path.IndexTextDir}/dictionary_{self.type}.jsonl"
+            self.posting_file = f"{Path.IndexTextDir}/posting_{self.type}.jsonl"
+        
+        self.initialize()
+        # print("Finished reading the index.")
 
-    def load_index(self):
-        """
-        Loads the index and posting data from disk into memory.
-        """
-        # Path to index and posting files
-        index_file = f"{Path.IndexWebDir}/index_{self.type}.txt"
-        posting_file = f"{Path.IndexWebDir}/posting_{self.type}.json"
-        docid_map_file = f"{Path.IndexWebDir}/docid_map_{self.type}.txt"
-
-        # Load docid_map (docNo to docId)
-        with open(docid_map_file, 'r') as f:
-            for line in f:
-                docNo, docid = line.strip().split()
-                docid = int(docid)
-                self.docid_map[docNo] = docid
-                self.docno_map[docid] = docNo
-
-        # Load index (term to list of docIds)
-        with open(index_file, 'r') as f:
-            for line in f:
-                term, doc_ids = line.strip().split(':')
-                doc_id_list = list(map(int, doc_ids.split(',')))
-                self.inverted_index[term] = doc_id_list
-
-        # Load postings (term to {docId: frequency})
-        with open(posting_file, 'r') as f:
+    def initialize(self):
+        with open(self.posting_file, 'r') as f:
             for line in f:
                 dic = json.loads(line)
-                term = dic["term"]
-                docid = dic["docId"]
-                freq = dic["freq"]
+                docNo, docId = dic["docNo"], dic["docId"]
+                docId = int(docId)
+                self.docid_map[docNo] = docId
+                self.docno_map[docId] = docNo
+                
+        '''
+        self.posting_list: 
+        {
+            term: {
+                docId: freq
+            }
+        }
+        '''
+        with open(self.dictionary_file, 'r') as f:
+            for line in f:
+                dic = json.loads(line)
+                term, docid, freq = dic["term"], dic["docId"], dic["freq"]
                 if term not in self.posting_list:
                     self.posting_list[term] = {}
                 self.posting_list[term][docid] = freq
+        
+    
+    # def load_docid_map(self):
+    #     with open(self.posting_file, 'r') as f:
+    #         for line in f:
+    #             docNo, docId = line.strip().split()
+    #             docId = int(docId)
+    #             self.docid_map[docNo] = docId
+    #             self.docno_map[docId] = docNo
+    #     # print the first element of the dictionary
+    #     print("First element of docid_map: ", list(self.docid_map.items())[0])
+    #     print("First element of docno_map: ", list(self.docno_map.items())[0])
+
+    
+    # def load_posting_list(self):
+    #     with open(self.dictionary_file, 'r') as f:
+    #         for line in f:
+    #             dic = json.loads(line)
+    #             term = dic["term"]
+    #             docid = dic["docId"]
+    #             freq = dic["freq"]
+    #             if term not in self.posting_list:
+    #                 self.posting_list[term] = {}
+    #             self.posting_list[term][docid] = freq
 
     # Returns the integer docId for a given docNo
     def getDocId(self, docNo):
-        """
-        Returns the integer `docId` corresponding to the given `docNo`.
-        If the docNo is not found, returns -1.
-        """
         return self.docid_map.get(docNo, -1)
 
     # Returns the string docNo for a given docId
-    def getDocNo(self, docId):
-        """
-        Returns the string `docNo` corresponding to the given `docId`.
-        If the docId is not found, returns -1.
-        """
-        return self.docno_map.get(docId, -1)
+    def getDocNo(self, docId) -> str:
+        return self.docno_map.get(docId, "")
 
     # Returns the document frequency (DF) for a given term
     def DocFreq(self, token):
-        """
-        Returns the document frequency (DF) for a given term.
-        This is the number of documents that contain the term.
-        """
-        if token in self.inverted_index:
-            return len(self.inverted_index[token])
+        if token in self.posting_list:
+            return len(self.posting_list[token])
         return 0
 
     # Returns the collection frequency (CF) for a given term
     def CollectionFreq(self, token):
-        """
-        Returns the collection frequency (CF) for a given term.
-        This is the total number of occurrences of the term in the entire collection.
-        """
         if token in self.posting_list:
             return sum(self.posting_list[token].values())
         return 0
 
     # Returns the posting list for a given term
     def getPostingList(self, token):
-        """
-        Returns the posting list for a given term.
-        The posting list is a dictionary where the key is `docId` and the value is the frequency of the term in that document.
-        """
         return self.posting_list.get(token, {})
--- Assignment2/Indexing/MyIndexWriter.py
+++ Assignment2/Indexing/MyIndexWriter.py
@@ -1,43 +1,33 @@
 import Classes.Path as Path
 import json
+# import jsonlines
 
 class MyIndexWriter:
     def __init__(self, type):
-        """
-        Initializes the index writer.
-        `type` specifies the type of document (e.g., 'trecweb' or 'trectext').
-        The writer will create an index file, a docid_map file, and a posting file.
-        """
         self.docid_map = {}  # key: docNo, value: docId
-        self.inverted_index = {}  # key: term, value: posting list (docId, frequency)
+        self.inverted_index = {}  # key: term, value: posting list (docId, [appearance locator])
         self.counter = 0  # Used to assign unique docIds
-        self.buffer_limit = 1000  # Limit for the number of documents in memory before flushing
+        self.buffer_limit = 30000  # Limit for the number of documents in memory before flushing
         self.buffer_count = 0  # Track the number of documents processed so far
         self.type = type
 
         if type == "trecweb":
-            self.index_file = open(f"{Path.IndexWebDir}/index_{self.type}.txt", "w")
-            self.posting_file = open(f"{Path.IndexWebDir}/posting_{self.type}.json", "w")
-            self.docid_map_file_path = f"{Path.IndexWebDir}/docid_map_{self.type}.txt"
+            self.dictionary_file = open(f"{Path.IndexWebDir}/dictionary_{self.type}.jsonl", "w") # [term, docId, freq]
+            self.posting_file = open(f"{Path.IndexWebDir}/posting_{self.type}.jsonl", "w") # docNo: docId
         elif type == "trectext":
-            self.index_file = open(f"{Path.IndexTextDir}/index_{self.type}.txt", "w")
-            self.posting_file = open(f"{Path.IndexTextDir}/posting_{self.type}.json", "w")
-            self.docid_map_file_path = f"{Path.IndexTextDir}/docid_map_{self.type}.txt"
+            self.dictionary_file = open(f"{Path.IndexTextDir}/dictionary_{self.type}.jsonl", "w")
+            self.posting_file = open(f"{Path.IndexTextDir}/posting_{self.type}.jsonl", "w")
 
         
         return
 
     def index(self, docNo, content):
-        """
-        Builds the index for each document.
-        Converts `docNo` to a unique integer `docId`, and indexes the terms in the document.
-        """
         # For every docNo, assign a unique docId
         if docNo not in self.docid_map:
             self.docid_map[docNo] = self.counter
             self.counter += 1
 
-        docid = self.docid_map[docNo]
+        docid = self.docid_map[docNo] # {docNo: docId}
 
         # Count the frequency of each term
         terms = content.split()  # Get terms
@@ -60,43 +50,19 @@ class MyIndexWriter:
             self.refresh_buffer()
 
     def refresh_buffer(self):
-        """
-        Flushes the in-memory inverted index to the disk (index and posting files).
-        """
         for term, postings in self.inverted_index.items():
-            # Write each term and its document list to the index file (terms and docIds)
-            self.index_file.write(f"{term}: {','.join(str(docid) for docid, _ in postings)}\n")
-
             # Write the posting list to the posting file (docId and frequency)
             for docid, freq in postings:
-                # write in json format
-                # {term: term, docId: docId, freq: freq}
-                # json.dump({"term": term, "docId": docid, "freq": freq}, self.posting_file)
-                self.posting_file.write(json.dumps({"term": term, "docId": docid, "freq": freq}) + "\n")
-                
+                self.dictionary_file.write(json.dumps({"term": term, "docId": docid, "freq": freq}) + "\n")
+        
+        for docNo, docId in self.docid_map.items():
+            self.posting_file.write(json.dumps({"docNo": docNo, "docId": docId}) + "\n")
 
-        # Clear the in-memory index after flushing
         self.inverted_index.clear()
-        self.buffer_count = 0
-
-    def save_docid_map(self):
-        """
-        Writes the `docid_map` (mapping between `docNo` and `docId`) to the disk.
-        """
-        with open(self.docid_map_file_path, 'w') as f:
-            for docNo, docId in self.docid_map.items():
-                f.write(f"{docNo} {docId}\n")
+        self.docid_map.clear()
+        self.buffer_count = 0 # reInit
 
     def close(self):
-        """
-        Closes the index writer. Flushes any remaining data in memory, and writes the docid_map to disk.
-        """
         if self.buffer_count > 0:
             self.refresh_buffer()  # Flush the remaining buffered data
-
-        # Write the docid_map to a separate file
-        self.save_docid_map()
-
-        # Close the index and posting files
-        self.index_file.close()
-        self.posting_file.close()
+        self.dictionary_file.close()
